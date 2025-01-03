60b24234191fd3c8eed62347ce464e3fa168f384
pranayapradhan <112082386+pranayapradhan@users.noreply.github.com>
2025-01-03 17:52:46 +0000

    Conversion Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = 32
+
+console.log(typeof score);
+score = "Pranaya"
+console.log(typeof(score));
+
+let scoreInNumber = Number(score)
+console.log(typeof(scoreInNumber));
+console.log(scoreInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0
+
+let isLoggedIn = "Pranaya"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "value" => true
+// "" => false
+
+
+let random = 34
+
+let stringRandom = String(random)
+console.log(stringRandom);
+console.log(typeof stringRandom);
+
