9f1ac9a67b3a9726dd8989a838cbd17610202d30
pranayapradhan <112082386+pranayapradhan@users.noreply.github.com>
2025-01-03 18:17:10 +0000

    Conversion and Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = 32
 
-console.log(typeof score);
+// console.log(typeof score);
 score = "Pranaya"
-console.log(typeof(score));
+// console.log(typeof(score));
 
 let scoreInNumber = Number(score)
-console.log(typeof(scoreInNumber));
-console.log(scoreInNumber);
+// console.log(typeof(scoreInNumber));
+// console.log(scoreInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -16,8 +16,8 @@ console.log(scoreInNumber);
 let isLoggedIn = "Pranaya"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "value" => true
@@ -27,6 +27,40 @@ console.log(typeof booleanIsLoggedIn);
 let random = 34
 
 let stringRandom = String(random)
-console.log(stringRandom);
-console.log(typeof stringRandom);
+// console.log(stringRandom);
+// console.log(typeof stringRandom);
 
+// **************** Operations ******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3**3);
+// console.log(3/3);
+// console.log(3%3);
+
+let str1 = "hello"
+let str2 = " Pranaya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 // this kind of code is less recomended to use
