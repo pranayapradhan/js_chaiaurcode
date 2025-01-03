142bebc7a8c828cd1ebc397da0118a9b04231fc5
pranayapradhan <112082386+pranayapradhan@users.noreply.github.com>
2025-01-03 17:15:29 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all code as a newer version
+
+//alert(3 + 3) //we are using node js not browser
+
+console.log(3
+    + 3
+); //code readability should be high so don't do this
+console.log("Pranaya");
+
+
+
+let name = "Pranaya"
+let age = 21
+let isLoggedIn = true
+let state = null
+
+// number => 2 to power 32
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
