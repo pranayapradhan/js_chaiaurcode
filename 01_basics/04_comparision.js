3ed0b2c0902fe6a48dbc28b213c3feea28573757
pranayapradhan <112082386+pranayapradhan@users.noreply.github.com>
2025-01-04 17:09:03 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 < 3);
+// console.log(2 > 3);
+// console.log(2 == 3);
+// console.log(2 != 3);
+// console.log(2 >= 3);
+
+// console.log("2" > 0);
+// console.log("02" == 2);
+
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined < 0);
+
+
+// === strict comparision
+
+console.log("2" === 2);
+console.log(2 === 2);
