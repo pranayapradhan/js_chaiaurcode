da5bf56d344ca1ad4aa534214441a38db283f6d7
pranayapradhan <112082386+pranayapradhan@users.noreply.github.com>
2025-01-06 18:12:35 +0000

    Number and math done

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,21 +1,42 @@
 const score = 100
-console.log(score);
+// console.log(score);
 
 const balance = new Number(10)
-console.log(balance);
+// console.log(balance);
 
-console.log(typeof score);
-console.log(typeof balance);
+// console.log(typeof score);
+// console.log(typeof balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2)); // 10.00
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); // 10.00
 
 
 const otherNumber = 123.8966
 
-console.log(otherNumber.toPrecision(4));
-console.log(otherNumber.toFixed(2));
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toFixed(2));
 
 const hundreds = 10000000
-console.log(hundreds.toLocaleString());
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ******************** Maths ************************
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(7.5));
+// console.log(Math.ceil(7.5));
+// console.log(Math.floor(7.5));
+
+
+console.log(Math.random()); // gives a random value between 0 and 1
+console.log((Math.random() * 10) + 1 ); // If I want a random value between 1 and 9
+console.log(Math.round(Math.random() * 10) + 1 ); // If I want a random value between 1 and 9 with round
+
+
+const min = 15
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min)); // between min and max value 
+
