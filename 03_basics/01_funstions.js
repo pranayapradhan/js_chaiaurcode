d979697f80b5ebfdf06e0d1813075513df32381f
pranayapradhan <112082386+pranayapradhan@users.noreply.github.com>
2025-01-09 12:43:52 +0000

    objects and array in functions

--- 03_basics/01_funstions.js
+++ 03_basics/01_funstions.js
@@ -40,5 +40,41 @@ function LoginUserMessage(username = "Sam"){
     return `${username} just logged in.`
 }
 
-console.log(LoginUserMessage("Pranaya"));
+// console.log(LoginUserMessage());
+// console.log(LoginUserMessage("Pranaya"));
+
+
+// function calculateCartPrice(...num1){ // ... is rest operator which return an array of element
+//     return num1
+// }
+
+function calculateCartPrice(val1, val2, ...num1){ // here first two element are passed on val1 and val2 and another elements are passed on num1 as array
+}
+
+// console.log(calculateCartPrice(200, 3000, 4000, 3400));
+
+
+const user = {
+    username: "Pranaya",
+    price: 599
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username: "Pradhan",
+    price: 499
+})
+
+const myNewArray = [404, 808, 403, 405]
+
+function returnFirstValue(getArray){
+    return getArray[0]
+}
+
+// console.log(returnFirstValue(myNewArray));
+console.log(returnFirstValue([100, 200, 300, 400]));
 
