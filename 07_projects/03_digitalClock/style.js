b419957a2238b433b33f3371578e886fca3820a1
pranayapradhan <112082386+pranayapradhan@users.noreply.github.com>
2025-01-18 17:14:01 +0000

    Real time digital clock

---
+++ 07_projects/03_digitalClock/style.js
@@ -0,0 +1,8 @@
+const clock = document.getElementById('clock')
+
+
+setInterval( function() {
+    let date = new Date()
+    clock.innerHTML = date.toLocaleTimeString()
+    
+}, 1000) //1000 is milisecond to run the funciton
